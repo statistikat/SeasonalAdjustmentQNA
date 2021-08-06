279a578814e16ecef8f94beebb6e1d3fa641617a
Julia Knöbl <Julia.Knoebl@statistik.gv.at>
2021-08-06 15:28:34 +0200

    change path to developing

--- Step1_LoadInput.R
+++ Step1_LoadInput.R
@@ -1,4 +1,4 @@
-release <- T
+release <- F
 # if release = T take released data from github repo, for development take unpublished data from upstream project
 
 if(release){
@@ -8,6 +8,6 @@ if(release){
 }
 
 sapply(c("T101", "T102", "T103", "T110", "T111", "T117", "T120", "T121"), function(t){
-  load(file.path("data", paste0(t, "TS.Rds")), envir = .GlobalEnv)
+  load(file.path(dataPath, paste0(t, "TS.Rds")), envir = .GlobalEnv)
 })
 
