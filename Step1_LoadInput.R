2a20c8eeb836172f99ad82fd19e6520d10579460
Julia Knöbl <Julia.Knoebl@statistik.gv.at>
2021-08-06 15:25:27 +0200

    data paths changed

--- Step1_LoadInput.R
+++ Step1_LoadInput.R
@@ -1,3 +1,12 @@
+release <- T
+# if release = T take released data from github repo, for development take unpublished data from upstream project
+
+if(release){
+  dataPath <- file.path("data")
+} else{
+  dataPath <- file.path("../data_unpublished")
+}
+
 sapply(c("T101", "T102", "T103", "T110", "T111", "T117", "T120", "T121"), function(t){
   load(file.path("data", paste0(t, "TS.Rds")), envir = .GlobalEnv)
 })
